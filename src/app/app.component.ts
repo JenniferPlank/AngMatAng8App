794a4c8b4a499df2d10211c42766b650b286699d
Jennifer Plank <jenplank@rogers.com>
2020-02-11 15:56:05 -0500

    Looking Pretty Good Feb 11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,9 @@
 import { Component } from '@angular/core';
-import { AnimalService } from './animal.service';
-import { Observable } from 'rxjs';
-import { Animal, FilterState, Filter, Option } from './types';
-
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  animals;
-  filterState: FilterState;
-  filters: Observable<Filter[]>;
-
-  constructor(animalService: AnimalService) {
-    this.animals = animalService.animals;
-    this.filterState = animalService.filterState;
-    this.filters = animalService.filters;
-  }
-
-  changeFilter(category: string, option: Option) {
-    this.filterState[category] = option;
-  }
 }
